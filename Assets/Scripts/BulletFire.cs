9473f5640ec9326d6ef063972238ba7a65efc33d
DenisPavlenko <denispavlenko7610new@gmail.com>
2021-01-20 22:25:12 +0200

    add bullet script

---
+++ Assets/Scripts/BulletFire.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFire : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+    [SerializeField] Vector3 spawnPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Create();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Create()
+    {
+        Instantiate(bullet, spawnPosition ,Quaternion.identity);
+    }
+}
